92069601f3f5d0183a9da3d8003fc8d44b0f8a59
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-12-30 16:36:28 -0700

    half implementation of model i.e. vgg16 architecture done

--- model.py
+++ model.py
@@ -8,6 +8,12 @@ class Model:
 		'conv1_2_W': tf.get_variable('conv1_2_W', shape=[3,3,64,64], dtype=tf.float32),
 		'conv2_1_W': tf.get_variable('conv2_1_W', shape=[3,3,64,128], dtype=tf.float32),
 		'conv2_2_W': tf.get_variable('conv2_2_W', shape=[3,3,128,128], dtype=tf.float32),
+		'conv3_1_W': tf.get_variable('conv3_1_W', shape=[3,3,128,256], dtype=tf.float32),
+		'conv3_2_W': tf.get_variable('conv3_2_W', shape=[3,3,256,256], dtype=tf.float32),
+		'conv3_3_W': tf.get_variable('conv3_3_W', shape=[3,3,256,256], dtype=tf.float32),
+		'conv4_1_W': tf.get_variable('conv4_1_W', shape=[3,3,256,512], dtype=tf.float32),
+		'conv4_2_W': tf.get_variable('conv4_2_W', shape=[3,3,512,512], dtype=tf.float32),
+		'conv4_3_W': tf.get_variable('conv4_3_W', shape=[3,3,512,512], dtype=tf.float32),
 	}
 	biases = {
 		'conv0_1_b': tf.get_variable('conv0_1_b', shape=[3], dtype=tf.float32),
@@ -15,6 +21,12 @@ class Model:
 		'conv1_2_b': tf.get_variable('conv1_2_b', shape=[64], dtype=tf.float32),
 		'conv2_1_b': tf.get_variable('conv2_1_b', shape=[128], dtype=tf.float32),
 		'conv2_2_b': tf.get_variable('conv2_2_b', shape=[128], dtype=tf.float32),
+		'conv3_1_b': tf.get_variable('conv3_1_b', shape=[256], dtype=tf.float32),
+		'conv3_2_b': tf.get_variable('conv3_2_b', shape=[256], dtype=tf.float32),
+		'conv3_3_b': tf.get_variable('conv3_3_b', shape=[256], dtype=tf.float32),
+		'conv4_1_b': tf.get_variable('conv4_1_b', shape=[512], dtype=tf.float32),
+		'conv4_2_b': tf.get_variable('conv4_2_b', shape=[512], dtype=tf.float32),
+		'conv4_3_b': tf.get_variable('conv4_3_b', shape=[512], dtype=tf.float32),
 	}
 
 	def __init__(self, batch_size, dim_1, dim_2, num_input_channels=1, num_output_channels=2, learning_rate=0.001, num_epochs=100):
@@ -39,8 +51,8 @@ class Model:
 		conv1_2 = tf.nn.conv2d(relu1_1, Model.weights['conv1_2_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv1_2_b']
 		relu1_2 = tf.nn.relu(conv1_2)
 
-		hyper1_2 = tf.contrib.layers.batch_norm(relu1_2)
-		pool1 = tf.nn.max_pool(hyper1_2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+		hyper1 = tf.contrib.layers.batch_norm(relu1_2)
+		pool1 = tf.nn.max_pool(hyper1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 		conv2_1 = tf.nn.conv2d(pool1, Model.weights['conv2_1_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv2_1_b']
 		conv2_1 = tf.contrib.layers.batch_norm(conv2_1)
@@ -48,5 +60,31 @@ class Model:
 		conv2_2 = tf.nn.conv2d(relu2_1, Model.weights['conv2_2_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv2_2_b']
 		relu2_2 = tf.nn.relu(conv2_2)
 
-		hyper2_2 = tf.contrib.layers.batch_norm(relu2_2)
-		pool2 = tf.nn.max_pool(hyper2_2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+		hyper2 = tf.contrib.layers.batch_norm(relu2_2)
+		pool2 = tf.nn.max_pool(hyper2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+		conv3_1 = tf.nn.conv2d(pool2, Model.weights['conv3_1_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv3_1_b']
+		conv3_1 = tf.contrib.layers.batch_norm(conv3_1)
+		relu3_1 = tf.nn.relu(conv3_1)
+		conv3_2 = tf.nn.conv2d(relu3_1, Model.weights['conv3_2_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv3_2_b']
+		conv3_2 = tf.contrib.layers.batch_norm(conv3_2)
+		relu3_2 = tf.nn.relu(conv3_2)
+		conv3_3 = tf.nn.conv2d(relu3_2, Model.weights['conv3_3_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv3_3_b']
+		relu3_3 = tf.nn.relu(conv3_3)
+
+		hyper3 = tf.contrib.layers.batch_norm(relu3_3)
+		pool3 = tf.nn.max_pool(hyper3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+		conv4_1 = tf.nn.conv2d(pool3, Model.weights['conv4_1_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv4_1_b']
+		conv4_1 = tf.contrib.layers.batch_norm(conv4_1)
+		relu4_1 = tf.nn.relu(conv4_1)
+		conv4_2 = tf.nn.conv2d(relu4_1, Model.weights['conv4_2_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv4_2_b']
+		conv4_2 = tf.contrib.layers.batch_norm(conv4_2)
+		relu4_2 = tf.nn.relu(conv4_2)
+		conv4_3 = tf.nn.conv2d(relu4_2, Model.weights['conv4_3_W'], strides=[1,1,1,1], padding='SAME') + Model.biases['conv4_3_b']
+		relu4_3 = tf.nn.relu(conv4_3)
+
+		hyper4 = tf.contrib.layers.batch_norm(relu4_3)
+		pool4 = tf.nn.max_pool(hyper4, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+
